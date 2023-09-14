6497a2a0179108c865fd91b4f41a43e35ef03647
박민기 <86062121+mingki1242@users.noreply.github.com>
2023-09-14 11:57:03 +0900

    1_change

--- 1_homework/3.js
+++ 1_homework/3.js
@@ -1,3 +1,4 @@
+/*
 // 희소 행렬을 나타내는 객체 생성 함수
 function create_matrix(n, x) {
     const matrix = [];
@@ -52,3 +53,36 @@ const C = sum(A, B, 5);
 console.log(A);
 console.log(B);
 console.log(C);
+*/
+function create_matrix(n,x)
+{
+    let matrix = []
+    let before_position = []
+    while(matrix.length < n)
+    {
+        let row = Math.floor(Math.random() * n);
+        let col = Math.floor(Math.random() *n);
+        let value = Math.floor(Math.random()*n);
+        if(before_position[0] !== row && before_position[1] !== col)
+        {
+            matrix.push({row , col , value})
+        }
+    }
+
+
+
+    return matrix
+}
+
+function sum(A,B)
+{
+
+}
+
+
+const A = create_matrix(5, 4);
+const B = create_matrix(5, 4);
+
+console.log(A);
+
+
